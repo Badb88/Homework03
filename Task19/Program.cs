d4c819670d866c6f7bce41933862aed0f0694875
OlegB <os.babkin@gmail.com>
2022-05-24 21:09:50 +0300

    Добавил решение с while

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = number;
+int result = 0;
+while (number > 0)
+{
+    int x = number % 10;
+    result = result * 10 + x;
+    number = number / 10;
+}
+if (result==i)
+{
+    Console.WriteLine("Является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Не является палиндромом.");
+}
