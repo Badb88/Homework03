8141c32d7ce7cfd2935053e1df21794b294d3fd3
OlegB <os.babkin@gmail.com>
2022-06-01 18:17:35 +0300

    Добвлено решение с возведением в куб

---
+++ Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+Console.Write($"{number} - > ");
+for (int i = 1; i <= number; i++)
+{
+    result = i * i * i;
+    Console.Write(result);
+if (i!=number)
+{
+    Console.Write(" , ");
+}
+}
